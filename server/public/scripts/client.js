abe82526457df4278572d89914530a4aeafaff39
Adam Bergstrom <abx1138@gmail.com>
2023-01-04 09:18:39 -0600

    completed get route / table

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,29 @@
 $(document).ready(onReady);
 
 function onReady() {
-  console.log("hi. let's make a CRUD app.");
+  console.log("JS_Troll_Builder_CRUD");
+  fetchAndRenderTroll()
+}
+
+function fetchAndRenderTroll(){
+  console.log('in fetchAndRenderTroll');
+  $.ajax({
+    type: 'GET',
+    url: '/troll'
+  }).then(function(trolls) {
+    $('#trollTable').empty();
+    console.log(trolls);
+    for(let troll of trolls){
+      $('#trollTable').append(`
+      <tr data-id=${troll.id}>
+        <td>${troll.name}</td>
+        <td>${troll.notes}</td>
+        <td>${troll.head}</td>
+        <td>${troll.body}</td>
+      </tr>
+      `);
+    }
+  }).catch((error) => {
+    console.log('error in GET', error);
+});
 }
--- server/routes/troll.router.js
+++ server/routes/troll.router.js
@@ -3,6 +3,45 @@ const trollRouter = express.Router();
 
 // DB CONNECTION
 const pool = require('../modules/pool.js'); 
+// *****
+
+// GET
+trollRouter.get('/', (req, res) => {
+    console.log('in get');
+    let sqlQuery = `
+    SELECT * FROM "troll" 
+        ORDER BY "name";
+    `;
+    pool.query(sqlQuery)
+        .then((dbRes) => {
+            res.send(dbRes.rows);
+        })
+        .catch((dbErr) => {
+            console.log('error getting trolls', dbErr);
+        });
+});
+
+// todoRouter.get('/', (req, res) => {
+//     console.log('in get');
+//     let sqlQuery = `
+//       SELECT * FROM "todo" 
+//         ORDER BY "id";
+//     `;
+//     pool.query(sqlQuery)
+//       .then((dbRes) => {
+//         res.send(dbRes.rows);
+//       })
+//       .catch((dbErr) => {
+//         console.log('error getting books', dbErr);
+//         res.sendStatus(500);
+//       });
+//   });
+// POST
+
+// PUT
+
+// DELETE
 
 
+// *****
 module.exports = trollRouter;
